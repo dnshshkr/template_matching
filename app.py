efb8cb28f96cb76b0c3c249bcc28ee98fe92b905
delloydrnd5 <delloydrnd5@gmail.com>
2025-03-03 17:23:09 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -12,28 +12,33 @@ GPIO_TRIGGER_PIN = 23
 GPIO_OK_PIN = 27
 GPIO_NG_PIN = 22
 GPIO_LOGGING_PIN = 24
-OK_THRESHOLD = 0.7
+OK_THRESHOLD = 0.1
 LOG_PATH = '/home/pi/template_matching/logs'
 global_live_image = zeros((1, 1, 3), dtype='uint8')
-global_triggered_image = zeros((1, 1, 3), dtype='uint8')
+data = {
+    'timestamp': 0,
+    'image': zeros((1, 1, 3), dtype='uint8')
+}
 def trigger_callback(*args):
-    global global_triggered_image
-    print('Triggered')
-    print(args)
+    global data
     image = global_live_image.copy()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) if len(image.shape) == 3 else image
-    global_triggered_image = image.copy()
-    score = 1.0 - max(0, cv2.minMaxLoc(cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)[1]))
+    score = max(0, cv2.minMaxLoc(cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED))[1])
     status = score >= OK_THRESHOLD
     gpio.gpio_write(h, GPIO_OK_PIN, status)
     gpio.gpio_write(h, GPIO_NG_PIN, not status)
+    print('Status:', 'OK' if status else 'NG')
     print('Score:', score)
+    data = {
+        'timestamp': args[3],
+        'image': image.copy()
+    }
 
 def logging_callback(*args):
     timestamp = args[3]
-    name = f'{datetime.fromtimestamp(timestamp).strftime("%d/%m/%Y.%H:%M")}.jpg'
+    name = f'{datetime.fromtimestamp(timestamp).strftime("%Y%m%d_T%H%M%S")}.jpg'
     os.makedirs(LOG_PATH, exist_ok=True)
-    cv2.imwrite(f'{LOG_PATH}/{name}', global_triggered_image)
+    cv2.imwrite(f'{LOG_PATH}/{name}', data['image'])
     print('Logged:', name)
 
 def check_template():
@@ -67,16 +72,23 @@ def setup():
     gpio.gpio_claim_alert(h, GPIO_LOGGING_PIN, gpio.RISING_EDGE)
     gpio.callback(h, GPIO_LOGGING_PIN, gpio.RISING_EDGE, logging_callback)
     gpio.gpio_write(h, GPIO_READY_PIN, True if check_template() else False)
-    capture = Capture(NAME, 0)
+    capture = Capture(NAME, 0, threaded=True)
     capture.start()
 
 def loop():
     global global_live_image
     global_live_image = capture.get_frame(None)
-    cv2.imshow(NAME, global_live_image)
+    live_ratio = global_live_image.shape[1] / global_live_image.shape[0]
+    live_resized = cv2.resize(global_live_image, (int(template.shape[0] * live_ratio), template.shape[0]))
+    canvas = cv2.hconcat([cv2.cvtColor(template, cv2.COLOR_GRAY2BGR), live_resized])
+    cv2.imshow(NAME, canvas)
     key = cv2.waitKey(1) & 0xff
     if key == ord('S'.lower()):
-        Thread(target=save_template).start()
+        save_template()
+    elif key == ord('T'.lower()):
+        Thread(target=trigger_callback, args=(None, None, None, datetime.today().timestamp())).start()
+    elif key == ord('L'.lower()):
+        Thread(target=logging_callback, args=(None, None, None, datetime.today().timestamp())).start()
 
 if __name__ == '__main__':
     setup()
